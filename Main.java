a3feb8685a5b3eb6108a7425efb2ff82e81acb15
Yashas_J <yashasjraj@gmail.com>
2023-11-27 19:23:32 +0530

    Array basic #2

--- Main.java
+++ Main.java
@@ -1,19 +1,59 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 class Main {
-    public static void main(String[] args) {
-        int[] arr = new int[10]; // statuc array
 
-        arr[0] = 10;
-        arr[1] = 11;
-        arr[2] = 12;
+    public void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    };
+
+    public int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] > pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = arr[high];
+        arr[high] = temp;
+        return i;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+
+        int[] arr = new int[10]; // static array
+
+        int[] arr2 = { 8, 9, 4, 5, 6, 7 };
 
         // Static array Length
         int len = arr.length;
-
         System.out.println(len);
 
+        // to sort a Static/normal array we use Arrays class
+        Arrays.sort(arr); // ascending order
+
         for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+        // desending order (using quick sort)
+        m.quickSort(arr2, 0, arr2.length - 1);
+
+        for (int i : arr2) {
             System.out.print(i + " ");
         }
 
@@ -36,5 +76,6 @@ class Main {
 
         // remove element by index
         arr1.remove(0);
+
     }
 }
