cd975a9e09c385c21658cad0a9bb149a0662737d
Yashas_J <yashasjraj@gmail.com>
2023-11-28 14:02:15 +0530

    Linked List added

---
+++ linkedList.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class linkedList {
+    public static void main(String[] args) {
+
+        // Collection framework linkedlist
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("Yashas");
+        ll.add("is");
+        ll.add("op");
+
+        System.out.println(ll);
+    }
+}
